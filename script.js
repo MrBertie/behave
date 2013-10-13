85a0d9817c9b5bb1b3bb1e20744ce16c2dd0d409
Hendry Badao <hendrybadao@gmail.com>
2013-10-13 21:10:39 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,31 @@
+/* DOKUWIKI:include behave.js */
+
+var plugin_behave = (function () {
+
+    // public vars/methods
+    var pub = {};
+
+    pub.editor = null;
+
+    pub.init = function () {
+        editor = new Behave({
+            textarea: document.getElementById('wiki__text'),
+            replaceTab: true,
+            softTabs: true,
+            tabSize: 4,
+            autoOpen: true,
+            overwrite: true,
+            autoStrip: true,
+            autoIndent: true,
+            fence: false
+        });
+    };
+
+    // return only public methods/properties
+    return pub;
+})();
+
+
+jQuery(function() {
+    plugin_behave.init();
+});
